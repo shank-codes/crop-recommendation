b4ffc41c8e6c5831c27c5e6c3d5b7349bc9ceb51
Saurav <saurav.n.shetty2@gmail.com>
2021-04-26 21:59:00 +0530

    feed forward nn implementation

---
+++ feedforwardnn.py
@@ -0,0 +1,91 @@
+from random import seed
+from random import random
+from math import exp
+
+# Initialize a network
+# each node is implemented using dictionary
+# each layer is a list of nodes, and network is a list of layers
+def initialize_network(n_inputs, n_hidden, n_outputs):
+	network = list()
+	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+	network.append(output_layer)
+	return network
+
+seed(1)
+network = initialize_network(2, 1, 2)
+for layer in network:
+	print(layer)
+
+# Calculate neuron activation for an input
+# basically weighted sum of inputs to the neuron
+def activate(weights, inputs):
+	activation = weights[-1]
+	for i in range(len(weights)-1):
+		activation += weights[i] * inputs[i]
+	return activation
+
+# Transfer neuron activation
+# sigmoid function
+def transfer(activation):
+	return 1.0 / (1.0 + exp(-activation))
+
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+	inputs = row
+	for layer in network:
+		new_inputs = [] # store input for next layer/ output of this layer
+
+        # traverse through each node and calculate its output
+		for neuron in layer: # each neuron is a dictionary
+			activation = activate(neuron['weights'], inputs)
+			neuron['output'] = transfer(activation) # storing output in the dictionary/neuron
+			new_inputs.append(neuron['output'])
+
+		inputs = new_inputs
+
+	return inputs
+
+
+# test forward propagation
+network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+		[{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
+row = [1, 0, None] # None represents the bias input, it can be deleted 
+output = forward_propagate(network, row)
+print(output)
+
+
+# Calculate the derivative of an neuron output
+def transfer_derivative(output):
+	return output * (1.0 - output)
+
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+	for i in reversed(range(len(network))):
+		layer = network[i]
+		errors = list()
+		if i != len(network)-1:
+            # for hidden layers
+			for j in range(len(layer)):
+				error = 0.0
+				for neuron in network[i + 1]:
+					error += (neuron['weights'][j] * neuron['delta'])
+				errors.append(error)
+		else:
+            # for output layer
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(expected[j] - neuron['output'])
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+# test backpropagation of error
+network = [[{'output': 0.7105668883115941, 'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+		[{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]}, {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
+expected = [0, 1]
+backward_propagate_error(network, expected)
+for layer in network:
+	print(layer)
