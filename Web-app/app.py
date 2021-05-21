29dee70979abea9cbb189724344219b5d77c2acb
shank-codes <shashankhiremat22@gmail.com>
2021-05-21 22:37:45 +0530

    "fixed bugs"

--- Web-app/app.py
+++ Web-app/app.py
@@ -6,21 +6,8 @@ app = Flask(__name__)
 
 @app.route('/',methods=["GET","POST"])
 def hello_world():
-    '''
-    n,p,k,temp,humidity,ph,rainfall = 0,0,0,0.0,0.0,0.0,0.0
-    if request.method=='POST':
-        n = int(request.form['nitrogen'])
-        p = int(request.form['phosphorous'])
-        k = int(request.form['potassium'])
-        temp = float(request.form['temperature'])
-        humidity = float(request.form['humidity'])
-        ph = float(request.form['ph'])
-        rainfall = float(request.form['rainfall'])
-        #res = model.lr.predict((model.scaler.transform([[battery,bluetooth,clkspee]])))
-        '''
     return render_template('index.html')
-    #return render_template(n,p,k,temp,humidity,ph,rainfall)
-
+    
 @app.route('/predict',methods=["POST"])
 def recommend():
     n = int(request.form['nitrogen'])
